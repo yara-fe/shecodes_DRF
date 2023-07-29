a60c423b09e3102233a06b29cd61b3bee49e08f1
yara-fe <yarafe95@gmail.com>
2023-07-29 12:01:57 +1000

    Added ProjectDetail view, urls

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.ProjectList.as_view()),  
+    path('projects/', views.ProjectList.as_view()),
+    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns) 
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -10,3 +10,22 @@ class ProjectList(APIView):
         projects = Project.objects.all() #gets all data from DB
         serializer = ProjectSerializer(projects, many=True) #converts projects into json
         return Response(serializer.data) #sends response as a json
+    
+    #defines POST request - creating a project
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data) #loads request data into serializer
+        if serializer.is_valid():
+            serializer.save() #save a new record to DB
+            return Response(serializer.data) #respond with JSON with save details
+        
+class ProjectDetail(APIView):
+
+    #Returns object from the DB that has matching PK in the request
+    def get_object(self, pk):
+            return Project.objects.get(pk=pk)
+
+    #defines GET request for a particular record/ Primary Key
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
